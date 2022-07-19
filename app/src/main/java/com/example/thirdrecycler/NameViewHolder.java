a836abe894d25ce29d9bf5e31db100a0594a2adf
princessnuri <nurgaeva1606@gmail.com>
2022-07-19 18:48:03 +0600

    Initial commit

---
+++ app/src/main/java/com/example/thirdrecycler/NameViewHolder.java
@@ -0,0 +1,19 @@
+package com.example.thirdrecycler;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class NameViewHolder extends RecyclerView.ViewHolder {
+    private TextView textView;
+    public NameViewHolder(@NonNull View itemView) {
+        super(itemView);
+        textView=itemView.findViewById(R.id.textview_name);
+    }
+
+    public void bind(String name){
+        textView.setText(name);
+    }
+}
